3273d84d20bc3dbed367cc7f50ace8c3fd8fb14c
ragnagord <piter.cupakabra@gmail.com>
2015-02-25 17:10:45 +0000

    remove category

--- js/app.js
+++ js/app.js
@@ -8,4 +8,21 @@ angular.module('myApp',["ui.router", "myApp.factoryCategoriesService"])
  $scope.addCategory = function(category){
             CategoriesService.addCategory(category);
         }
+ $scope.removeCategory = function(){
+            CategoriesService.removeCategory($scope.currentCategory.name);
+        }
+        
+$scope.currentCategory = null;
+
+function setCurrentCategory(category) {
+            $scope.currentCategory = category;
+            $scope.currentBookmark = null;
+        }
+        
+function isCurrentCategory(category) {
+            return $scope.currentCategory !== null && category.name === $scope.currentCategory.name;
+        }
+        
+$scope.setCurrentCategory = setCurrentCategory;
+$scope.isCurrentCategory = isCurrentCategory;
  })
